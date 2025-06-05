753d378554d091f7a89e8187ad543f71c0c74272
duanduansweetie <duanduansweetie@outlook.com>
2025-06-05 22:05:22 +0800

    fix..

--- CM/src/stores/course.js
+++ CM/src/stores/course.js
@@ -1,29 +1,24 @@
 import { defineStore } from 'pinia'
 
-export const useCourseStore = defineStore('course', {
-  state: () => ({
-    currentExerciseId: null,
-    currentSubmissionId: null,
-    retryExerciseId: null,     // 新增：错题重做的练习ID
-    retrySubmissionId: null,   // 新增：错题重做的提交ID
-  }),
+import { ref } from 'vue'
 
-  actions: {
-    setCurrentExerciseId(id) {
-      this.currentExerciseId = id
-    },
-    setCurrentSubmissionId(id) {
-      this.currentSubmissionId = id
-    },
-    // 新增：设置错题重做相关信息
-    setRetryInfo(exerciseId, submissionId) {
-      this.retryExerciseId = exerciseId
-      this.retrySubmissionId = submissionId
-    },
-    // 新增：清除错题重做信息
-    clearRetryInfo() {
-      this.retryExerciseId = null
-      this.retrySubmissionId = null
-    }
+export const useCourseStore = defineStore('course', () => {
+  // 当前选中的课程ID
+  const currentCourseId = ref('')
+  //选中的在线练习id
+   const currentExerciseId = ref('')
+  // 设置当前课程ID的方法
+  function setCurrentCourseId(courseId) {
+    currentCourseId.value = courseId
   }
-})
+  function setCurrentExerciseId(exerciseId) {
+    currentExerciseId.value = exerciseId
+  }
+  return {
+    currentCourseId,
+    setCurrentCourseId,
+     currentExerciseId,
+    setCurrentExerciseId,
+  }
+}
+)
